2128c1b3de917c8aff267723183bc5c901818eb3
finch2kt <finch2kt@Dukes.jmu.edu>
2017-02-20 04:04:08 -0500

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,15 @@
+/**
+ * This is a java Hello World program
+ *
+ *@author Kashaun Finch
+ *@verion V1.0, 2/20/17
+ */
+public class HelloWorld
+{
+   public static void main(String[] args)
+  {
+    // Prints "Hello, World" to the terminal window.
+    System.out.println("Hello, World");
+    
+  }
+}
